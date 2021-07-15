d00748accb98b7411bc7022145975f08bf9aa870
Longfish-Meng <loongfish@foxmail.com>
2021-07-14 23:31:22 -0700

    done the leap year problem

--- lab1/LeapYear.java
+++ lab1/LeapYear.java
@@ -1,5 +1,5 @@
 /** Class that determines whether or not a year is a leap year.
- *  @author YOUR NAME HERE
+ *  @author C.M.
  */
 public class LeapYear {
 
@@ -12,6 +12,17 @@ public class LeapYear {
         } else {
             System.out.printf("%d is not a leap year.\n", year);
         }
+    }
+    
+    /** Check whether the year is a leap year.
+     *  @source 
+     */
+    public static boolean isLeapYear(int year) {
+        if (year % 400 == 0)
+            return true;
+        if (year % 4 == 0 && year % 100 != 0)
+            return true;
+        return false;
     }
 
     /** Must be provided an integer as a command line argument ARGS. */
